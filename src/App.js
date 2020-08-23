a6ddb8a5cebaec6b7611176b630b4701859e0200
Tyler Ink Broeker <tylerinkbroeker@gmail.com>
2020-08-22 22:56:18 -0700

    Initial commit with nonfunctional navbar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import NavBar from './compontents/NavBar';
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
